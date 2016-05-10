7b17476c4da9cf01a36c6d3ba9d2e1bbb65221f6
Robert Rouhani <robert.rouhani@gmail.com>
2016-05-10 00:35:38 -0400

    Enhanced fb parser, got 5000 posts from HH and 2500 from LSH (facebook limits)

--- fb_build_json_input.js
+++ fb_build_json_input.js
@@ -1,7 +1,16 @@
 var fs = require('fs');
 var csv_parse = require('csv-parse');
 
-var outfb = JSON.parse(fs.readFileSync('OUTFB.json', 'utf8'));
+if (process.argv.length < 4)
+{
+	console.log("USAGE: nodejs fb_build_json_input.js [IN FILE] [OUT FILE]");
+	return;
+}
+
+var in_file_name = process.argv[2];
+var out_file_name = process.argv[3];
+
+var outfb = JSON.parse(fs.readFileSync(in_file_name, 'utf8'));
 var emoji_dict = {};
 
 var base_emoji_parser = csv_parse(fs.readFileSync('fb-emoticons.csv', 'utf8'), { delimiter: ',', quote: '7', relax_column_count: true}, function(err, data) {
@@ -15,7 +24,7 @@ var base_emoji_parser = csv_parse(fs.readFileSync('fb-emoticons.csv', 'utf8'), {
 });
 
 base_emoji_parser.on('end', function() {
-    console.log(emoji_dict);
+    //console.log(emoji_dict);
     var out_json = { name: "Hackathon Hackers", children: [] };
     var out_emoji_dict = {};
 
@@ -26,30 +35,39 @@ base_emoji_parser.on('end', function() {
         for (var user_message_idx in user.messages) {
             var user_message = user.messages[user_message_idx].message;
             for (var emoji in emoji_dict) {
-                if (user_message.indexOf(emoji) != -1) {
-                    console.log("found emoji" + emoji);
+				var emoji_str_index = user_message.indexOf(emoji);
+                if (emoji_str_index != -1) {
+					
+					//a limitation of this technique are links, http:// contains :/ which is the "unsure" emoji.
+					if (user_message.substring(emoji_str_index - 4, emoji_str_index) == "http" || user_message.substring(emoji_str_index - 5, emoji_str_index) == "https") {
+						console.log("skipping link");
+						continue;
+					}
+					
+					console.log(user_message.substring(emoji_str_index - 5, emoji_str_index + 5));
+					
                     var emoji_name = emoji_dict[emoji];
-                    if (out_emoji_dict.hasOwnProperty(emoji_name))
-                        out_emoji_dict[emoji_name] += 1;
+                    if (user_emoji_dict.hasOwnProperty(emoji_name))
+                        user_emoji_dict[emoji_name] += 1;
                     else
-                        out_emoji_dict[emoji_name] = 1;
+                        user_emoji_dict[emoji_name] = 1;
                 }
             }
         }
         
-        //if (Object.keys(user_emoji_dict).length == 0)
-            //continue;
+        if (Object.keys(user_emoji_dict).length == 0)
+            continue;
         
-        //for (var user_emoji in user_emoji_dict) {
-          //  user_node.children.push({name: user_emoji, size: user_emoji_dict[user_emoji]});
-        //}
+        for (var user_emoji in user_emoji_dict) {
+            user_node.children.push({name: user_emoji, size: user_emoji_dict[user_emoji]});
+        }
         
-        //out_json.children.push(user_node);
+        out_json.children.push(user_node);
     }
     
-    for (var out_emoji in out_emoji_dict) {
-            out_json.children.push({name: out_emoji, size: out_emoji_dict[out_emoji]});
-        }
+    //for (var out_emoji in out_emoji_dict) {
+      //      out_json.children.push({name: out_emoji, size: out_emoji_dict[out_emoji]});
+        //}
 
-    fs.writeFileSync('hh.json', JSON.stringify(out_json, null, '\t'));
+    fs.writeFileSync(out_file_name, JSON.stringify(out_json, null, '\t'));
 });
--- fb_scrape.js
+++ fb_scrape.js
@@ -5,46 +5,63 @@
 var fs = require('fs');
 var FB = require('fb');
 
+if (process.argv.length != 4) {
+	console.log("USAGE: nodejs fb_scrape [MAX POSTS] [GROUP ID]")
+	return;
+}
+
 //output data buffer
 var outdata = { users: [] };
+var cur_posts = 0;
+var max_posts = process.argv[2];
+var group_id = process.argv[3]; //"759985267390294";
 
-console.log("Making FB request...");
-
-//Get a token (and explore the API) here: https://developers.facebook.com/tools/explorer/
-FB.setAccessToken('');
-
-//Start making a facebook request
-FB.api(
-	'/759985267390294/feed',
-	'GET',
-	{"fields":"id,message,updated_time,from", "limit": "1000"},
-	function (response) {
-
-		console.log("Parsing response...");
+function parseFb(response) {
+	if (!response || response.error) {
+		console.log(!response ? 'unknown error' : response.error);
+	}
 	
-		//iterate through each message in the response
-		response.data.forEach(function(message) {
-			if (message.message === undefined)		//skip what I assume are deleted messages. Some messages in /feed end up just being a timestamp and a unique ID, we don't want those
-				return;
-			
-			//sorting by user, so see if the user is currently 
-			var userFound = outdata.users.filter(function(i) {
-				return i.id == message.from.id;
-			})[0];
-			
-			//either append to the existing user id, or generate a new user with a single message in it.
-			if (userFound === undefined) {
-				outdata.users.push({id: message.from.id, messages: [ {message: message.message, time: message.time, id: message.id} ]});
-			}
-			else {
-				userFound.messages.push({message: message.message, time: message.time, id: message.id});
-			}
-		});
+	console.log("Parsing response...");
+	
+	//iterate through each message in the response
+	response.data.forEach(function(message) {
+		if (message.message === undefined)		//skip what I assume are deleted messages. Some messages in /feed end up just being a timestamp and a unique ID, we don't want those
+			return;
 		
+		cur_posts += 1;
+		
+		//sorting by user, so see if the user is currently 
+		var userFound = outdata.users.filter(function(i) {
+			return i.id == message.from.id;
+		})[0];
+		
+		//either append to the existing user id, or generate a new user with a single message in it.
+		if (userFound === undefined) {
+			outdata.users.push({id: message.from.id, messages: [ {message: message.message, time: message.updated_time, id: message.id} ]});
+		}
+		else {
+			userFound.messages.push({message: message.message, time: message.updated_time, id: message.id});
+		}
+	});
+	
+	console.log(cur_posts + " / " + max_posts);
+	
+	if (cur_posts < max_posts)
+	{
+		console.log("Making FB request...");
+		//console.log(response.paging.next);
+		FB.api(
+			'/' + group_id + '/feed',
+			{fields: ['id','message','updated_time','from'], limit: 1000, offset: cur_posts},
+			parseFb);
+		return;
+	}
+	else
+	{
 		console.log("Writing to file...");
 		
 		//Write out to a file
-		fs.writeFile("./OUTFB.json", JSON.stringify(outdata, null, 4), function(err) {
+		fs.writeFile("./fb_data/" + group_id + ".json", JSON.stringify(outdata, null, 4), function(err) {
 			if (err) {
 				return console.log(err);
 			}
@@ -52,4 +69,18 @@ FB.api(
 			console.log("File saved");
 		});
 	}
+}
+
+console.log("Making FB request...");
+
+//Get a token (and explore the API) here: https://developers.facebook.com/tools/explorer/
+FB.setAccessToken('EAACEdEose0cBACAoa1gv5eNuyhlPSVM2ZCwehlXsbtzZCXbZB6i5ekzN5ROksQTkSUQEIZCcrJA4f1XtL7q474EbVmu1K3ZC1jlz0rqMSsjAibTRNCx6cxvnQE4EhkEQC8ZCUXzX9LBezefGDQ0pQJNPW07klAk56RBAfEOyJ66gZDZD');
+
+FB.options({version: 'v2.6'});
+
+//Start making a facebook request
+FB.api(
+	'/' + group_id + '/feed',
+	{fields: ['id','message','updated_time','from'], limit: 1000},
+	parseFb
 );
